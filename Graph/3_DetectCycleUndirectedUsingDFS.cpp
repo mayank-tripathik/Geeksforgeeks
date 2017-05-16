021b47d210e54239055b8789fcfc1cfffb6905f7
Mayank Kumar Tripathi <mayanktripathik@gmail.com>
2017-05-16 11:11:09 +0530

    Update and rename 3_DetectCycleUndirectedUsingDFS to 3_DetectCycleUndirectedUsingDFS.cpp

--- Graph/3_DetectCycleUndirectedUsingDFS.cpp
+++ Graph/3_DetectCycleUndirectedUsingDFS.cpp
@@ -1,29 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 vector<bool> visited;
-vector<int> parent;
-void dfs(vector<list<int> > &graph, int u){
+
+bool dfs(vector<list<int> > &graph, int u, int parent){
 	visited[u]=true;
 	cout<<"u:"<<u<<endl;
 	list<int>::iterator itr;
 	for(itr=graph[u].begin();itr!=graph[u].end();itr++){
 		int v=(*itr);
-		if(!visited[v]){
-			cout<<"v:"<<v<<endl;
-			parent[v]=u;
-			return dfs(graph,v);
-		}
-		else if(visited[v] && parent[u]!=v){
-			cout<<"Cycle exist\n";
-		}
+		if(!visited[v])
+			dfs(graph,v,u);
+		else if(v!=parent)
+			return true;
 	}
+	return false;
 }
 
-void hasCycle(vector<list<int> > &graph){
+bool hasCycle(vector<list<int> > &graph){
 	int n=graph.size();
 	visited.resize(n,false);
-	parent.resize(n,-1);
-	dfs(graph,1);
+	for(int i=0;i<n;i++){
+		cout<<"i:"<<i<<endl;
+		if(!visited[i]){
+			if(dfs(graph,i,-1))
+				return true;
+		}
+	}
+	return false;
 }
 
 int main(){
@@ -32,13 +35,16 @@ int main(){
 	while(test--){
 		int n,e;
 		cin>>n>>e;
-		vector<list<int> > graph(n+1);
+		vector<list<int> > graph(n);
 		for(int i=0;i<e;i++){
 			int u,v;
 			cin>>u>>v;
 			graph[u].push_back(v);
 			graph[v].push_back(u);
 		}
-		hasCycle(graph);
+		if(hasCycle(graph))
+			cout<<"has cycle\n";
+		else
+			cout<<"no cycle\n";
 	}
 }
