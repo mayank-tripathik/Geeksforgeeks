826c0ca704d77c8a3ae808e1e05f5eb5ca03c8fd
Mayank Kumar Tripathi <mayanktripathik@gmail.com>
2017-05-17 19:15:25 +0530

    Update 5_TopologicalSorted.cpp

--- Graph/5_TopologicalSorted.cpp
+++ Graph/5_TopologicalSorted.cpp
@@ -57,3 +57,63 @@ int main(){
 		}
 		topologicalSort(graph);
 }
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define LOWNODE 1
+int flag=true;
+void print(vector<int> &arr){
+	for(int i=0;i<arr.size();i++)
+		cout<<arr[i]<<" ";
+	cout<<endl;
+}
+
+void dfs(vector<list<int> >&graph, vector<int> &indegree, vector<int> &temp, vector<bool> &visited){
+	if(temp.size()==graph.size()-1){
+		print(temp);
+		flag=false;
+	}
+	for(int i=LOWNODE;i<graph.size() && flag;i++)
+	{
+		int u=i;
+		if(!visited[u] && indegree[u]==0){
+			visited[u]=true;
+			temp.push_back(u);
+			list<int>::iterator itr;
+			for(itr=graph[u].begin();itr!=graph[u].end();itr++){
+				int v=(*itr);
+				indegree[v]--;
+			}
+			dfs(graph,indegree,temp,visited);
+			for(itr=graph[u].begin();itr!=graph[u].end();itr++){
+				int v=(*itr);
+				indegree[v]++;
+			}
+			visited[u]=false;
+			temp.pop_back();
+		}
+	}
+}
+void topologicalSort(vector<list<int> >&graph, vector<int> &indegree){
+	int n=graph.size();
+	vector<int> temp;
+	vector<bool> visited(n,false);
+	//print(indegree);
+	//print(temp);
+	dfs(graph,indegree,temp,visited);
+}
+
+int main(){
+		int n,e;
+		cin>>n>>e;
+		vector<list<int> > graph(n+1);
+		vector<int> indegree(n+1,0);
+		for(int i=0;i<e;i++){
+			int u,v;
+			cin>>u>>v;
+			graph[u].push_back(v);
+			indegree[v]++;
+		}
+		topologicalSort(graph,indegree);
+}
