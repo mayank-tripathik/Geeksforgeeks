563d004395c0f60368920c0582c4f9712346a92c
Mayank Kumar Tripathi <mayanktripathik@gmail.com>
2017-05-19 01:31:33 +0530

    Update cycleDirectedGraph.cpp

--- Graph/cycleDirectedGraph.cpp
+++ Graph/cycleDirectedGraph.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<bool> visited;
-vector<int> recstack;
+vector<int> color;
 #define AUTO list<int>::iterator
 
 enum color
@@ -12,7 +11,6 @@ enum color
 
 bool dfsIterative(vector<list<int> > &graph, int u){
 	int n=graph.size();
-	vector<int> color(n,WHITE);
 	stack<int> s;
 	s.push(u);
 	while(!s.empty())
@@ -41,32 +39,28 @@ bool dfsIterative(vector<list<int> > &graph, int u){
 	return false;
 }
 
-bool dfs(vector<list<int> > &graph, int u){
-	visited[u]=true;
-	cout<<"u:"<<u<<endl;
-	list<int>::iterator itr;
-	recstack[u]=true;
+bool dfsColor(vector<list<int> > &graph, int u){
+	AUTO itr;
+	color[u]=GRAY;
 	for(itr=graph[u].begin();itr!=graph[u].end();itr++){
 		int v=(*itr);
-		if(!visited[v] && dfs(graph,v)){
+		if(color[v]==WHITE && dfsColor(graph,v)){
 			return true;
 		}
-		else if(recstack[v]){
+		else if(color[v]==GRAY){
 			return true;
 		}
 	}
-	recstack[u]=false;
+	color[u]=BLACK;
 	return false;
 }
 
 bool hasCycle(vector<list<int> > &graph){
 	int n=graph.size();
-	visited.resize(n,false);
-	recstack.resize(n,false);
+	color.resize(n,WHITE);
 	for(int i=0;i<n;i++){
-		cout<<"i:"<<i<<endl;
-		if(!visited[i]){
-			if(dfs(graph,i))
+		if(color[i]==WHITE){
+			if(dfsColor(graph,i))
 				return true;
 		}
 	}
