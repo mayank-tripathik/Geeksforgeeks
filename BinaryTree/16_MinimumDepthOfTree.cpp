22984a3fcdf2d6889addf29398068a373ff3b511
Mayank Kumar Tripathi <mayanktripathik@gmail.com>
2017-06-06 17:49:24 +0530

    Update 16_MinimumDepthOfTree.cpp

--- BinaryTree/16_MinimumDepthOfTree.cpp
+++ BinaryTree/16_MinimumDepthOfTree.cpp
@@ -3,7 +3,7 @@
  * The minimum depth is the number of nodes along the shortest path from root node down to the nearest leaf node.
  * For every node, wecan find the minimum depth of its left and right subtree. If one of the subtree is NULL, we can 
  * simply return the other one
- * Following is a recusrive implementation
+ * WE can code the above observation in recursive code
  * TC:O(n)
  * Note that this approach runs for whole tree, even if shorteds leaf node is found much before
  * We can use iterative approach, where we do level order traversal and break as soon as we got a leaf node
@@ -25,7 +25,7 @@ public:
 	
 };
 
-int minDepth(TreeNode* root){
+int minDepthRecursive(TreeNode* root){
 	// If tree is empty return 0
 	if(root==NULL)
 		return 0;
@@ -34,14 +34,35 @@ int minDepth(TreeNode* root){
 		return 1;
 	// Else if node has only left child recur for it
 	if(root->left && !root->right)
-		return minDepth(root->left)+1;
+		return minDepthRecursive(root->left)+1;
 	// Else if node has only right child recur for it
 	if(!root->left && root->right)
-		return minDepth(root->right)+1;
+		return minDepthRecursive(root->right)+1;
 	// Else if node has both left and right child, recur for it and take minimum of them
-	return min(minDepth(root->left),minDepth(root->right))+1;
+	return min(minDepthRecursive(root->left),minDepthRecursive(root->right))+1;
 }
 
+int minDepthIterative(TreeNode* root){
+	queue<TreeNode*> q;
+	q.push(root);
+	int depth=0;
+	while(!q.empty()){
+		depth++;
+		int n=q.size();
+		while(n--){
+			TreeNode* parent=q.front();
+			q.pop();
+			if(parent->left==NULL && parent->right==NULL)
+				return depth;
+			if(parent->left)
+				q.push(parent->left);
+			if(parent->right)
+				q.push(parent->right);
+		}
+
+	}
+	return depth;
+}
 
 int main(){
 	TreeNode *root=new TreeNode(10);
@@ -52,5 +73,5 @@ int main(){
 	root->right->left=new TreeNode(4);
 	root->right->left->right=new TreeNode(9);
 	root->right->right=new TreeNode(23);
-	cout<<minDepth(root)<<endl;
+	cout<<minDepthIterative(root)<<endl;
 }
