d1d07710127cc5c6929be24595631c9e137c1c17
Mayank Kumar Tripathi <mayanktripathik@gmail.com>
2017-06-19 09:47:39 +0530

    Update 31_DiagonalTraversal.cpp

--- BinaryTree/31_DiagonalTraversal.cpp
+++ BinaryTree/31_DiagonalTraversal.cpp
@@ -23,6 +23,61 @@ public:
 	
 };
 
+void diagonalTraversalIterative(TreeNode* root)
+{
+	if(root == NULL)
+		return;
+
+	queue<TreeNode*> que;
+
+	que.push(root);
+
+    //add the root of the tree to the queue..
+	while(!que.empty())
+	{
+		int size = que.size();
+        /*
+         The idea here is to take the left most node of each diagonal 
+         into the queue and print out all the nodes lying on the right
+         of this node(nodes to the right in the same diagonal).
+         in the above example:
+         8 is the leftmost node of the first diagonal. So we print all
+         the nodes on the right of this node in this diagonal. So we print
+         10 and 14.
+         In this process we add all the immediate left child of these 
+         nodes into the queue since they are the leftmost nodes of
+         the next consecutive diagonals.
+         
+         We use the size variable to keep track the number of nodes in 
+         the queue that belong to the current diagonal.
+         in the above example:
+         once we print the first diagonal 8, 10, 14 the queue will have 2
+         nodes 3 and 13 which are the leftmost nodes of the
+         next diagonal. So we use these to print the corresponding nodes.
+         In this process we will add nodes 1, 4 into the queue. So if we
+         dont have the size of the queue, before pusing these new nodes
+         then we will not be able to track the elemets that belong to this
+         diagonal. For this purpose we use the variable size in our program.
+         
+         Hope this helps..
+         */
+		while(size--)
+		{
+			TreeNode* temp = que.front();
+			que.pop();
+
+			while(temp)
+			{
+				cout << temp -> value << " ";
+				if(temp -> left)
+					que.push(temp -> left);
+				temp = temp -> right;
+			}
+		}
+		cout << endl;
+	}
+}
+
 // This map uses inorder traversal to traverse the tree. Any traversal can be used as
 // long as the slop that is passed is set correctly
 void setMap(TreeNode* root, int slope,map<int,vector<int> > &hash){
@@ -76,4 +131,5 @@ int main(){
 	root->left->right->right=new TreeNode(7);
 	root->right->right->left=new TreeNode(13);
 	diagonalTraversal(root);
+	diagonalTraversalIterative(root);
 }
