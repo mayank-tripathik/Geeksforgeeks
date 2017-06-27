38fa789cf6a295bf169d37ffd5b09f14a808a8f5
Mayank Kumar Tripathi <mayanktripathik@gmail.com>
2017-06-28 00:05:32 +0530

    Update designPattern.cpp

--- OOP/designPattern.cpp
+++ OOP/designPattern.cpp
@@ -1,1 +1,6 @@
 http://www.cpphub.com/
+https://herbsutter.com/elements-of-modern-c-style/
+https://akrzemi1.wordpress.com/
+http://cpptruths.blogspot.in/
+http://www.lonecpluspluscoder.com/
+http://codergears.com/Blog/?p=1957
