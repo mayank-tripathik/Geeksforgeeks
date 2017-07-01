654b070946f993d5a42a3c32ce750a51adaa7745
Mayank Kumar Tripathi <mayanktripathik@gmail.com>
2017-07-01 15:47:18 +0530

    Update 33_SearchElementInSortedRotatedArray.cpp

--- Array/33_SearchElementInSortedRotatedArray.cpp
+++ Array/33_SearchElementInSortedRotatedArray.cpp
@@ -24,7 +24,7 @@
        if (high < low)  return -1;
        if (high == low) return low;
 
-       int mid = (low + high)/2;   /*low + (high - low)/2;*/
+       int mid = (low + high)/2;   //low + (high - low)/2;
        if (mid < high && arr[mid] > arr[mid + 1])
            return mid;
        if (mid > low && arr[mid] < arr[mid - 1])
